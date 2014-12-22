ff5154016efbb8055ffaad55a837c9f0d967d9c1
Kota FUTAMI <futami_k@worksap.co.jp>
2014-12-22 13:43:22 +0900

    Fix packing command error

--- package.sh
+++ package.sh
@@ -1,3 +1,3 @@
 # !/bin/sh
 echo [chatrello package] Package target directory to publish in store.
-zip chatrello-`git log -1 --format="%H"` target/
+zip chatrello-`git log -1 --format="%H"` target/*
